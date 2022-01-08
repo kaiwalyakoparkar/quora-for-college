7bd6528bc3936280d5b89a0a90b739ebaf1e7f3b
kaiwalyakoparkar <kaiwalyakoparkar@gmail.com>
2022-01-08 14:27:42 +0530

    feat: :memo: Added comments to partition the file into logical units

--- server/server.js
+++ server/server.js
@@ -1,6 +1,23 @@
+//--------- Including all the external packages -----------
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
+require('dotenv').config();
+
+//--------- Importing internal modules and files ----------
+const data = require(path.join(__dirname, './data/dev-data/testResponse.json'));
+
+//--------- Variable assignment ------------------
+const PORT = process.env.PORT || 8080;
 const app = express();
 
-app.listen(3000,() => {
-	console.log('Server has started on http://localhost:3000/');
+//--------- Functional code for this file ---------
+app.get('/', (req, res, next) => {
+	res.status(200).json(data);
+})
+
+app.listen(PORT,() => {
+	console.log(`Server has started on http://localhost:${PORT}/`);
 });
+
+//--------- Post function Assignment ---------------
