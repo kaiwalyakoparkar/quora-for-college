51e87e2c7e329ca92f1e9a75e7879f02f2652864
kaiwalyakoparkar <kaiwalyakoparkar@gmail.com>
2022-01-13 18:39:46 +0530

    feat: :lock: Security added to API through middlewares

--- server/app.js
+++ server/app.js
@@ -1,6 +1,16 @@
 //--------- Including all the external packages -----------
+//Node Js required
 const express = require('express');
+//Debugging
 const morgan = require('morgan');
+//Security
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
+const compression = require('compression');
+const cors = require('cors');
 
 //--------- Importing internal modules and files ----------
 const questionsRoutes = require('./routes/questionsRoutes.js');
@@ -12,9 +22,42 @@ const appError = require('./util/appError.js');
 const app = express();
 
 //--------- Middlewares ----------------
+app.use(cors());
+app.options('*', cors());
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
+app.use(helmet());
+
 if(process.env.NODE_ENV === 'development') {
 	app.use(morgan('dev'));
 }
+
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60*60*1000,
+  message: 'Too many request from this IP please try again later after an hour'
+});
+app.use('/api', limiter);
+
+app.use(express.json({ limit: '10kb' }));
+app.use(mongoSanitize());
+app.use(xss());
+app.use(hpp({
+  whitelist: [
+    'duration',
+    'ratingQuantity',
+    'ratingAverage',
+    'maxGroupSize',
+    'difficulty',
+    'price'
+  ]
+}));
+
+app.use(compression());
 
 //--------- Functional code for this file ---------
 //Routes mounting
