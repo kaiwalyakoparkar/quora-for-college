7d99b5a498030eb7829e71f62cc563255b5a834a
kaiwalyakoparkar <kaiwalyakoparkar@gmail.com>
2022-01-14 19:27:34 +0530

    feat: :sparkles: User routes added

---
+++ server/routes/usersRoutes.js
@@ -0,0 +1,20 @@
+//Eternal imports
+const express = require('express');
+
+//Variable delarations
+const route = express.Router();
+
+route.use(express.json());
+
+const usersControllers = require('../controllers/usersControllers.js');
+const authControllers = require('../controllers/authControllers.js');
+
+route.post('/signup', authControllers.signup);
+route.post('/login', authControllers.login);
+
+route.route('/').get(authControllers.protect, authControllers.restrictTo('admin'), usersControllers.getAllUsers);
+route.route('/me').get(authControllers.protect, usersControllers.getMe, usersControllers.getSingleUser);
+route.route('/updateMe').patch(authControllers.protect, usersControllers.updateMe);
+route.route('/deleteMe').patch(authControllers.protect, usersControllers.deleteMe);
+
+module.exports = route;
