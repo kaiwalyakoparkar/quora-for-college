78e763120c0eb6970d7d62c6e623ebe461a76cfd
kaiwalyakoparkar <kaiwalyakoparkar@gmail.com>
2022-03-01 23:05:52 +0530

    feat: :rocket: Simple Navbar added

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,7 +1,17 @@
+//File Imports
+import Navbar from "../Navbar/Navbar";
+
+//Function handling the App renders
 export default function App() {
     return (
         <div>
-            <h1>Frontend development</h1>
+            {/*Rendering the Navbar function*/}
+            <Navbar />
+
+            {/*The application vody will fall under this div*/}
+            <div>
+                <h1>Frontend development</h1>
+            </div>
         </div>
     )
 }
---
+++ client/src/components/Navbar/Navbar.js
@@ -0,0 +1,31 @@
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+
+export default function Navbar() {
+    return (
+        <div>
+            {/*<h1>This is navbar</h1>*/}
+            <AppBar position="static">
+                <Toolbar>
+                <IconButton
+                    size="large"
+                    edge="start"
+                    color="inherit"
+                    aria-label="menu"
+                    sx={{ mr: 2 }}
+                >
+                    <MenuIcon />
+                </IconButton>
+                <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                    News
+                </Typography>
+                <Button color="inherit">Login</Button>
+                </Toolbar>
+            </AppBar>
+        </div>
+    )
+}
