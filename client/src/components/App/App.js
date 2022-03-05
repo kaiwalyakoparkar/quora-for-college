f758c62b136b492bc67adfb7397e86b10346d952
kaiwalyakoparkar <kaiwalyakoparkar@gmail.com>
2022-03-05 22:40:06 +0530

    feat: :zap: Added icon buttons in the navbar

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,10 +1,27 @@
+//External module imports
+import {createTheme, ThemeProvider} from '@mui/material/styles';
+
 //File Imports
 import Navbar from "../Navbar/Navbar";
+
+//Creating custom theme for the project
+//Creating override object
+const customTheme = createTheme({
+    palette: {
+      //Commented to use other components smoothly
+      primary: {
+        main: '#fffff'
+      },
+      secondary: {
+        main: '#6563ff'
+      }
+    }
+})
 
 //Function handling the App renders
 export default function App() {
     return (
-        <div>
+        <ThemeProvider theme={customTheme}>
             {/*Rendering the Navbar function*/}
             <Navbar />
 
@@ -12,6 +29,6 @@ export default function App() {
             <div>
                 <h1>Frontend development</h1>
             </div>
-        </div>
+        </ThemeProvider>
     )
 }
