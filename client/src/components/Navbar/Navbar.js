370ab6ce5bf60362b57917ce332496668819c6cc
kaiwalyakoparkar <kaiwalyakoparkar@gmail.com>
2022-03-06 10:30:14 +0530

    feat: :zap: Updated the layout of navbar

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -2,6 +2,8 @@ import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
@@ -16,35 +18,81 @@ export default function Navbar() {
             {/*<h1>This is navbar</h1>*/}
             <AppBar position="static" color="primary">
                 <Toolbar>
-                <IconButton
-                    size="large"
-                    edge="start"
-                    color="inherit"
-                    aria-label="menu"
-                    sx={{ mr: 2 }}
-                >
-                    <MenuIcon />
-                </IconButton>
-                <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                    QuFoCo
-                </Typography>
+                    <Grid container spacing={2}>
 
-                <IconButton color="secondary" size="large" edge="start" aria-label="home" component="span">
-                    <HomeOutlinedIcon />
-                </IconButton>
-                <IconButton color="secondary" size="large" edge="start" aria-label="home" component="span">
-                    <ExploreOutlinedIcon />
-                </IconButton>
-                <IconButton color="secondary" size="large" edge="start" aria-label="home" component="span">
-                    <FormatListBulletedOutlinedIcon />
-                </IconButton>
-                <IconButton color="secondary" size="large" edge="start" aria-label="home" component="span">
-                    <ChatBubbleOutlineIcon />
-                </IconButton>
-                <IconButton color="secondary" size="large" edge="start" aria-label="home" component="span">
-                    <NotificationsNoneOutlinedIcon />
-                </IconButton>
+                        {/*Left side of navbar*/}
+                        <Grid item xs={10}>
+                            <Box
+                                sx={{
+                                    display: 'flex',
+                                    flexDirection: 'row',
+                                    mr:2
+                                }}
+                            >
+                                <Typography 
+                                    variant="h6" 
+                                    component="h6" 
+                                    sx={{ 
+                                        mt:1,
+                                        mr:5
+                                    }}
+                                    >
+                                    QuFoCo
+                                </Typography>
 
+                                <IconButton 
+                                    color="secondary" 
+                                    size="large" 
+                                    edge="start" 
+                                    aria-label="home" 
+                                    component="span"
+                                >
+                                    <HomeOutlinedIcon />
+                                </IconButton>
+                                <IconButton 
+                                    color="secondary" 
+                                    size="large" 
+                                    edge="start" 
+                                    aria-label="explore" 
+                                    component="span"
+                                >
+                                    <ExploreOutlinedIcon />
+                                </IconButton>
+                                <IconButton 
+                                    color="secondary" 
+                                    size="large" 
+                                    edge="start" 
+                                    aria-label="list" 
+                                    component="span"
+                                >
+                                    <FormatListBulletedOutlinedIcon />
+                                </IconButton>
+                                <IconButton 
+                                    color="secondary" 
+                                    size="large" 
+                                    edge="start" 
+                                    aria-label="chat" 
+                                    component="span"
+                                >
+                                    <ChatBubbleOutlineIcon />
+                                </IconButton>
+                            </Box>
+                        </Grid>
+
+                        {/*right side of navbar*/}
+                        <Grid item xs={2}>
+                            <IconButton 
+                                    color="secondary" 
+                                    size="large" 
+                                    edge="start" 
+                                    aria-label="notification" 
+                                    component="span"
+                                >
+                                <NotificationsNoneOutlinedIcon />
+                            </IconButton>
+                        </Grid>
+
+                    </Grid>
                 </Toolbar>
             </AppBar>
         </div>
