3e98a876358a2382447987353b8f287b9ce97147
kaiwalyakoparkar <kaiwalyakoparkar@gmail.com>
2022-03-20 11:48:49 +0530

    feat: :zap: Initiated the file and folder structure

--- client/src/components/Home/HomeCenterContainer/HomeCenterContainer.js
+++ client/src/components/Home/HomeCenterContainer/HomeCenterContainer.js
@@ -1,15 +1,16 @@
 //Importing external files/modules
 import Typography from '@mui/material/Typography';
 
+//Importing internal files/modules
+import qdata from '../../../data/dev-data/questions.json';
+import QACard from '../../QACard/QACard';
+
+//Main container function
 export default function HomeCenterContainer () {
+    // console.log(qdata)
     return (
         <div>
-            <Typography
-                variant="h5" 
-                component="h5"
-            >
-                Center
-            </Typography>
+            <QACard/>
         </div>
     )
 }
---
+++ client/src/components/QACard/QACard.js
@@ -0,0 +1,16 @@
+//Importing external files/modules
+import Typography from '@mui/material/Typography';
+
+//Main QACard function
+export default function QACard () {
+    return (
+        <div>
+            <Typography
+                variant="h5" 
+                component="h5"
+                >
+                Lets go
+            </Typography>
+        </div>
+    )
+}
