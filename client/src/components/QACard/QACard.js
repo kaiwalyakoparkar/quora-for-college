382a21175f60dafa69bc76035325661750f0f4c8
kaiwalyakoparkar <kaiwalyakoparkar@gmail.com>
2022-03-20 11:56:32 +0530

    feat: :sparkles: Added the required assets to the module

--- client/src/components/QACard/QACard.js
+++ client/src/components/QACard/QACard.js
@@ -1,16 +1,25 @@
 //Importing external files/modules
+import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid';
+import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline';
+import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
+import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
+import Avatar from '@mui/material/Avatar';
 
 //Main QACard function
 export default function QACard () {
     return (
         <div>
-            <Typography
+            {/* <Typography
                 variant="h5" 
                 component="h5"
                 >
                 Lets go
-            </Typography>
+            </Typography> */}
+
+
+
         </div>
     )
 }
