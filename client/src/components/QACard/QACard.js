ccaa853d3c9b22910f2c5955073a658795883628
kaiwalyakoparkar <kaiwalyakoparkar@gmail.com>
2022-03-20 12:11:55 +0530

    feat: :rocket: Added layout box for qa card

--- client/src/components/QACard/QACard.js
+++ client/src/components/QACard/QACard.js
@@ -6,11 +6,23 @@ import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline';
 import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
 import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
 import Avatar from '@mui/material/Avatar';
+import { makeStyles } from '@mui/styles';
+
+//Creating custom styles
+const customStyle = makeStyles({
+    root: {
+        width: "fillParent",
+        height: 200,
+        backgroundColor: '#ffffff',
+    }
+})
 
 //Main QACard function
 export default function QACard () {
+    const classes = customStyle();
     return (
         <div>
+            
             {/* <Typography
                 variant="h5" 
                 component="h5"
@@ -18,8 +30,16 @@ export default function QACard () {
                 Lets go
             </Typography> */}
 
-
-
+            <Box
+                sx={{
+                    pr: 2.5,
+                    borderRadius: 5,
+                    boxShadow: 5
+                }}
+                className={classes.root}
+            >
+                
+            </Box>
         </div>
     )
 }
